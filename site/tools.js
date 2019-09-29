cab89a396536c8af90bfc50324d57e2a364085e3
Dakota Madden-Fong <maddenfong@gmail.com>
2019-09-29 12:04:24 -0700

    Update tools.js

--- site/tools.js
+++ site/tools.js
@@ -1,12 +1,12 @@
 var tools = {
     "DS1 Soul Level Calculator":{
         "description":"A tool to calculate the souls necessary to level up in Dark Souls 1.",
-        "link":"/DS1-Soul-Level-Calculator/",
+        "link":"DS1-Soul-Level-Calculator/",
         "github":"https://github.com/TrifectaIII/DS1-Soul-Level-Calculator"
     },
     "DS1 Class Calculator":{
         "description":"A tool to calculate the best starting class based on build goals in Dark Souls 1.",
-        "link":"/DS1-Class-Calculator/",
+        "link":"DS1-Class-Calculator/",
         "github":"https://github.com/TrifectaIII/DS1-Class-Calculator"
     }
 }
