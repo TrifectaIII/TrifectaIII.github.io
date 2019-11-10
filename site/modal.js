fe9713e41c2611f9cd5c44af0b1639795e17b6bb
Dakota Madden-Fong <maddenfong@gmail.com>
2019-11-09 18:41:03 -0800

    began work on dark mode toggle

--- site/modal.js
+++ site/modal.js
@@ -26,3 +26,16 @@ inner.addEventListener('click', function () {
 content.addEventListener('click', function (e) {
     e.stopPropagation();
 });
+
+var mode_toggle = document.querySelector('.mode-toggle');
+
+var dark_mode = false;
+
+mode_toggle.addEventListener('click',function () {
+    dark_mode = !dark_mode;
+    if (dark_mode) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+});
