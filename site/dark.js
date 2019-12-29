d88c6489112b307e12572580f3ce3bf4df99e0b8
Dakota Madden-Fong <maddenfong@gmail.com>
2019-12-29 09:22:22 -0800

    added cookies to remember dark mode state

--- site/dark.js
+++ site/dark.js
@@ -1,3 +1,5 @@
+var saved = cookie.get('dark-mode','off');
+
 var mode_toggle = document.querySelector('.mode-toggle');
 
 var dark_mode = false;
@@ -6,7 +8,14 @@ mode_toggle.addEventListener('click',function () {
     dark_mode = !dark_mode;
     if (dark_mode) {
         document.body.classList.add('dark-mode');
+        cookie.set('dark-mode','on');
     } else {
         document.body.classList.remove('dark-mode');
+        cookie.set('dark-mode','off');
     }
 });
+
+if (saved == 'on') {
+    document.body.classList.add('dark-mode');
+    dark_mode = true;
+}
