846e9d420eb60fd612ec251ba6cc919dab8c5c00
Dakota Madden-Fong <maddenfong@gmail.com>
2020-01-26 08:37:25 -0800

    improved comments and flow

--- site/dark.js
+++ site/dark.js
@@ -1,28 +1,31 @@
-// cookie.defaults.path = '/';
-
 // get cookie, and set default behaivior
 var saved = cookie.get('dark-mode','off');
 
-var mode_toggle = document.querySelector('.mode-toggle');
-
+// turn on if cookie indicates such
 var dark_mode = false;
-
-mode_toggle.addEventListener('click',function () {
-    dark_mode = !dark_mode;
-    if (dark_mode) {
-        document.body.classList.add('dark-mode');
-        cookie.set('dark-mode','on', {
-            path: '/',
-        });
-    } else {
-        document.body.classList.remove('dark-mode');
-        cookie.set('dark-mode','off', {
-            path: '/',
-        });
-    }
-});
 
 if (saved == 'on') {
     document.body.classList.add('dark-mode');
     dark_mode = true;
 }
+
+// get button from DOM
+var mode_toggle = document.querySelector('.mode-toggle');
+
+// toggle on button click
+mode_toggle.addEventListener('click',function () {
+    dark_mode = !dark_mode;
+    if (dark_mode) {
+        document.body.classList.add('dark-mode');
+        cookie.set('dark-mode','on', {
+            //save to root path so it works site-wide
+            path: '/',
+        });
+    } else {
+        document.body.classList.remove('dark-mode');
+        cookie.set('dark-mode','off', {
+            //save to root path so it works site-wide
+            path: '/',
+        });
+    }
+});
