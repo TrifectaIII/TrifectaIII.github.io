0993ee5a83c48eafc7c61a31672e8c5e8f2ea596
Dakota Madden-Fong <maddenfong@gmail.com>
2020-07-24 12:17:25 -0700

    fix to links

--- site/tools.js
+++ site/tools.js
@@ -1,17 +1,17 @@
 var tools = {
     "JavaScript Practice":{
         "description":"A tool to practice JavaScript, with problems to solve.",
-        "link":"JavascriptPractice/",
-        "github":"https://github.com/TrifectaIII/Javascript-Practice"
+        "link":"JavaScript-Practice/",
+        "github":"https://github.com/TrifectaIII/JavaScript-Practice",
     },
     "DS1 Soul Level Calculator":{
         "description":"A tool to calculate the souls necessary to level up in Dark Souls 1.",
         "link":"DS1-Soul-Level-Calculator/",
-        "github":"https://github.com/TrifectaIII/DS1-Soul-Level-Calculator"
+        "github":"https://github.com/TrifectaIII/DS1-Soul-Level-Calculator",
     },
     "DS1 Class Calculator":{
         "description":"A tool to calculate the best starting class based on build goals in Dark Souls 1.",
         "link":"DS1-Class-Calculator/",
-        "github":"https://github.com/TrifectaIII/DS1-Class-Calculator"
+        "github":"https://github.com/TrifectaIII/DS1-Class-Calculator",
     }
 }
